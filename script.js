a0a17f88d76dad00c5b6aa6a3e7f058abf22cf29
Dannycodes305 <89235491+Dannycodes305@users.noreply.github.com>
2021-11-27 20:25:45 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+
+function darkMode(){
+    document.getElementById('darkThemeActive').style.background='rgb(52, 38, 65)';
+    var h2=document.getElementById("darkModeText");
+    h2.style.color='white';
+
+}
+function lightMode(){
+    document.getElementById('darkThemeActive').style.background='white';
+    document.getElementById("darkModeText").style.color= 'black';
+
+}
+
+
+
